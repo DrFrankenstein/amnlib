DrFrankenstein/amnlib

commit d116d071fbfc698fcbeee7839c6ab7cb318d838e

Taking off the size hint from dAmnRichText.
No elegant enough way to calculate it for now, especially for tablumps
with parameters.

damnrichtext.cpp
@@ -217,8 +217,6 @@ QLinkedList<dAmnRichText::Element>::const_iterator dAmnRichText::parsedTablumps(
 QString dAmnRichText::toPlain() const
 {
     QString result;
-    result.reserve(this->sizehint);
-
     QStack<Element> state;
     QStack<uint> listcounts;
 

damnrichtext.h
@@ -52,7 +52,6 @@ public:
     };
 
 private:
-    int sizehint;
     QLinkedList<Element> tablumps;
 
     static QHash<QString, ElementType> map;

commit ad4be45be6ccea6e36c700b1a96ba8b5139799ef

@d116d07 broke the build. Time to go to bed.

damnrichtext.cpp
@@ -289,7 +289,6 @@ QString dAmnRichText::toPlain() const
 QString dAmnRichText::toHtml() const
 {
     QString result;
-    result.reserve(this->sizehint);
 
     Element el;
     foreach(el, this->tablumps)
@@ -408,7 +407,6 @@ QString dAmnRichText::toHtml() const
 QString dAmnRichText::toDAml() const
 {
     QString result;
-    result.reserve(this->sizehint);
 
     Element el;
     foreach(el, this->tablumps)